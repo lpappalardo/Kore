f940cbe6d30de3be556f20ef64c9c93378fe647f
lpappalardo <lautaro.pappalardo21@gmail.com>
2024-06-18 06:33:35 -0300

    Responsive nav

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,51 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false)
    
     return (
-        <header>
-            <div className="container encabezado">
-                <h1>Kore</h1>
-
-                <nav>
-                    <ul>
-                        <li className="navItemList"><NavLink className="navItem" to="/">Inicio</NavLink></li>
-                        <li className="navItemList"><NavLink className="navItem" to="/proyectos">Proyectos</NavLink></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-
         // <header>
         //     <div className="container encabezado">
+        //         <h1>Kore</h1>
+
         //         <nav>
-        //             <h1>
-        //                 <NavLink to="/" className="title">
-        //                 Kore
-        //                 </NavLink>
-        //             </h1>
-        //             <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
-        //               <span></span>
-        //               <span></span>
-        //               <span></span>
-        //             </div>
-        //             <ul className={menuOpen ? "open" : ""}>
-        //             <li className="navItemList">
-        //                 <NavLink className="navItem" to="/">Inicio</NavLink>
-        //             </li>
-
-        //             <li className="navItemList">
-        //                 <NavLink className="navItem" to="/proyectos">Proyectos</NavLink>
-        //             </li>
-
-        //             <li className="navItemList">
-        //                 <NavLink className="navItem" to="/proyectos">Perfil</NavLink>
-        //             </li>
+        //             <ul>
+        //                 <li className="navItemList"><NavLink className="navItem" to="/">Inicio</NavLink></li>
+        //                 <li className="navItemList"><NavLink className="navItem" to="/proyectos">Proyectos</NavLink></li>
         //             </ul>
         //         </nav>
         //     </div>
         // </header>
+
+        <header>
+            <div className="container encabezado">
+                <nav>
+                    <h1 className="navItemList">
+                        <NavLink  to="/" className="title navItem">
+                        Kore
+                        </NavLink>
+                    </h1>
+                    <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                    </div>
+                    <ul className={menuOpen ? "open" : ""}>
+                        <li className="navItemList">
+                            <NavLink className="navItem" to="/">Inicio</NavLink>
+                        </li>
+
+                        <li className="navItemList">
+                            <NavLink className="navItem" to="/proyectos">Proyectos</NavLink>
+                        </li>
+
+                        <li className="navItemList">
+                            <NavLink className="navItem" to="/proyectos">Perfil</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     )
 }
 
